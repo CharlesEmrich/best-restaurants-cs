8d02fae1d64e7c7a1f8021c619badafc5437312a
Charles Emrich <charles.emrich@gmail.com>
2017-06-07 16:20:07 -0700

    add code to pass update(int) test

--- Objects/Restaurant.cs
+++ Objects/Restaurant.cs
@@ -190,41 +190,41 @@ namespace BestRestaurants.Objects
     }
     public void Update(string columnName, int newInt)
     {
-      // SqlConnection conn = DB.Connection();
-      // conn.Open();
-      //
-      // SqlCommand cmd = new SqlCommand("", conn);
-      // if (columnName == "happy_hour") {
-      //   cmd = new SqlCommand("UPDATE restaurants SET happy_hour = @NewInt OUTPUT INSERTED.happy_hour WHERE id = @RestaurantId;", conn);
-      // }
-      // 
-      // SqlParameter newNameParameter = new SqlParameter();
-      // newNameParameter.ParameterName = "@NewInt";
-      // newNameParameter.Value = newInt;
-      // cmd.Parameters.Add(newNameParameter);
-      //
-      // SqlParameter restaurantIdParameter = new SqlParameter();
-      // restaurantIdParameter.ParameterName = "@RestaurantId";
-      // restaurantIdParameter.Value = this.GetId();
-      // cmd.Parameters.Add(restaurantIdParameter);
-      // SqlDataReader rdr = cmd.ExecuteReader();
-      //
-      // while(rdr.Read())
-      // {
-      //   if (columnName == "happy_hour") {
-      //     this._happyHour = rdr.GetBoolean(0);
-      //   }
-      // }
-      //
-      // if (rdr != null)
-      // {
-      //   rdr.Close();
-      // }
-      //
-      // if (conn != null)
-      // {
-      //   conn.Close();
-      // }
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("", conn);
+      if (columnName == "cuisine_id") {
+        cmd = new SqlCommand("UPDATE restaurants SET cuisine_id = @NewInt OUTPUT INSERTED.cuisine_id WHERE id = @RestaurantId;", conn);
+      }
+
+      SqlParameter newNameParameter = new SqlParameter();
+      newNameParameter.ParameterName = "@NewInt";
+      newNameParameter.Value = newInt;
+      cmd.Parameters.Add(newNameParameter);
+      
+      SqlParameter restaurantIdParameter = new SqlParameter();
+      restaurantIdParameter.ParameterName = "@RestaurantId";
+      restaurantIdParameter.Value = this.GetId();
+      cmd.Parameters.Add(restaurantIdParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        if (columnName == "cuisine_id") {
+          this._cuisineId = rdr.GetInt32(0);
+        }
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+
+      if (conn != null)
+      {
+        conn.Close();
+      }
     }
     public static Restaurant Find(int searchId)
     {
